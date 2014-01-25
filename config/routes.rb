a8ef41f8501d0c940533705fba7132e5e2e164ee
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-25 23:57:47 +0000

    finished step 9

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ LighthouseForum::Application.routes.draw do
 
   get 'posts',     to: 'posts#index'
   get 'posts/new', to: 'posts#new'
-  get 'posts/:id', to: 'posts#show'
+  get 'posts/:id/edit', to: 'posts#edit'
+  get 'posts/:id', to: 'posts#show', as: 'post'
+  patch 'posts/:id',    to: 'posts#update'
   post 'posts',    to: 'posts#create'
 
   # [tons of helpful comments]
