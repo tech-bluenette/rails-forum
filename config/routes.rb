628c935a58b7c99a303f1d4c25046b3f1d26d7b2
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-25 23:45:06 +0000

    finished step 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 LighthouseForum::Application.routes.draw do
 
-  get 'posts', to: 'posts#index'
+  get 'posts',     to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
   get 'posts/:id', to: 'posts#show'
+  post 'posts',    to: 'posts#create'
 
   # [tons of helpful comments]
   # The priority is based upon order of creation: first created -> highest priority.
